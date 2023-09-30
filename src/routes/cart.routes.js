20165946310b071bdf97788e36cbc2416c2f9cf9
Alejandro Bandi <bandi_alejandro@yahoo.com.ar>
2023-09-29 21:36:31 -0300

    Update cart.routes.js

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -2,15 +2,14 @@ import { Router } from "express";
 //import { CartManager } from "../CartManager.js";
 //import { cartModel } from "../models/Cart.js";
 import { findCarts, findOneCart, createCart, updateCart,deleteCart, updateQuantCart, emptyCart } from "../controllers/carts.controller.js";
-
+import applyPolicy from "../middleware/role.middleware.js";
 //const cartmanager = new CartManager ('carrito.txt')
 
 const cartRouter = Router()
 
 
 
-
-cartRouter.get('/', findCarts)
+cartRouter.get('/', applyPolicy (['admin']), findCarts)
 cartRouter.get('/:cid', findOneCart)
 cartRouter.post('/', createCart)
 cartRouter.post('/:cid/product/:pid', updateCart)
